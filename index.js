e4ee3af7f57b05d12c326370f7cf7a38764abb11
+ <+>
2022-06-15 22:47:17 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    const newArr = [];
+    for (let i = 0; i < array.length; i++) {
+        newArr.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+    return newArr;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+    console.log(n)
+    n--
+    }
+}
